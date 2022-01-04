47d8a2eb2ff007255df61f681dfffe710953a98a
gregbi <grigorybil@gmail.com>
2022-01-04 08:56:56 +0200

    add IUserSerivce Interface

---
+++ BLL.Abstractions/Interfaces/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Models;
+
+namespace BLL.Abstractions.Interfaces
+{
+    public interface IUserService
+    {
+        void Create(UserModel user);
+        void Delete(int id);
+        void Update(UserModel user);
+        IEnumerable<UserModel> Get();
+    }
+}
