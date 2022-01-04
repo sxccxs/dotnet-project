9f6d711342bcfbcd6c36bbaa96dd0a7a26e890e2
gregbi <grigorybil@gmail.com>
2022-01-04 08:59:15 +0200

    Minor model fixes

---
+++ Core/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class UserModel : BaseModel
+    {
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public string HashedPassword { get; set; }
+
+
+    }
+}
--- Core/Models/UserRegistrationModel.cs
+++ Core/Models/UserRegistrationModel.cs
@@ -2,13 +2,13 @@
 {
     public class UserRegistrationModel
     {
-        public string Username { get; set; }
+        public string UserName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public string RePassword { get; set; }
-        public UserRegistrationModel(string username, string email, string password, string rePassword)
+        public UserRegistrationModel(string userName, string email, string password, string rePassword)
         {
-            Username = username;
+            UserName = userName;
             Email = email;
             Password = password;
             RePassword = rePassword;
